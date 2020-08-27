44028cd066158a761e4b01136881359b0af7bc92
Heeyoon Koo <Heeyoon.koo-1@student.uts.edu.au>
2020-08-28 03:26:22 +0900

    Cus and Staff Sqls edited

--- MovieStore/web/db/CustomerTable.sql
+++ MovieStore/web/db/CustomerTable.sql
@@ -9,10 +9,17 @@
  */
 
 CREATE TABLE Customers (
-cEmail VARCHAR(50) not null primary key, 
-password VARCHAR(20) NOT NULL,
+cEmail VARCHAR(30) not null primary key, 
+password VARCHAR(18) NOT NULL,
 cusName VARCHAR(20) NOT NULL,
-DOB date NOT NULL,
+DOB VARCHAR(10) NOT NULL,
 gender INT NOT NULL,
 active boolean NOT NULL
 );
+/*Insert values*/
+INSERT INTO Customers(cEmail,password,cusName,DOB,gender,active)
+    VALUES('jane.doe@uts.edu.au','BA99342bob','Jane Doe','01/02/1990',1,false)
+,('helen.strokes@uts.edu.au','BA99342bob','Helen Strokes','11/12/1990',1,false)
+,('jim.carry@uts.edu.au','BA99342bob','Jim Carry','25/08/2000',0,false)
+,('steve.miller@uts.edu.au','BA99342bob','Steve Miller','03/11/1999',0,false)
+,('john.smith@uts.edu.au','BA99342bob','John Smith','10/06/1988',0,false);
--- MovieStore/web/db/StaffTable.sql
+++ MovieStore/web/db/StaffTable.sql
@@ -16,3 +16,10 @@ sRole VARCHAR(10) NOT NULL,
 gender INT NOT NULL,
 active boolean NOT NULL
 );
+/*Insert values*/
+INSERT INTO Staffs(sEmail,password,staName,sRole,gender,active)
+    VALUES('emily.doe@uts.edu.au','BA99342bob','Emily Doe','Manager',1,false)
+,('jeremy.strokes@uts.edu.au','BA99342bob','Jeremy Strokes','Manager',1,false)
+,('cameron.carry@uts.edu.au','BA99342bob','Cameron Carry','Manager',0,false)
+,('jeff.miller@uts.edu.au','BA99342bob','Jeff Miller','Manager',0,false)
+,('millie.smith@uts.edu.au','BA99342bob','Millie Smith','Admin',1,false);
