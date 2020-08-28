8d130238c48c0ae8af44c90a98c229ff021b9926
RinaYoo <62081885+RinaYoo@users.noreply.github.com>
2020-08-28 13:28:46 +1000

    Revert "loginservlet"
    This reverts commit 52c481d960b84ee03ae37dc96cef07c2a719540f.

--- MovieStore/src/java/uts/model/controller/LoginServlet.java
+++ MovieStore/src/java/uts/model/controller/LoginServlet.java
@@ -65,12 +65,35 @@ public class LoginServlet extends HttpServlet {
         } else {
             try {
                 // find user by email and password
-                customer = cusManager.loginCustomer(email, password);
+                user = manager.loginUser(email, password);
 
-                if (customer != null) {
-                    session.setAttribute("customer", customer);
+                if (user != null) {
+                    session.setAttribute("user", user);
                     // add access log
-                    cusManager.addLog(email);
+                    manager.addLog(email);
+
+                    // redirect user to the correct page
+                    String type = manager.getType(email);
+                    if (type.equals("customer")) {
+
+                        ArrayList<Product> display = productManager.fectProduct();
+
+                        request.setAttribute("display", display);
+                        request.getRequestDispatcher("main.jsp").include(request, response);
+//                        request.getRequestDispatcher("main.jsp").include(request, response);
+                    } else if (type.equals("staff")) {
+
+                        ArrayList<Product> display = productManager.fectProduct();
+
+                        request.setAttribute("display", display);
+                        request.getRequestDispatcher("staffMain.jsp").include(request, response);
+
+                    } else {
+//                        request.setAttribute("display", display);
+                        request.getRequestDispatcher("systemAdminMain.jsp").include(request, response);
+//                        String redirectURL = "/systemAdminMain.jsp";
+//                        response.sendRedirect(redirectURL);
+                    }
 
                 } else {
                     session.setAttribute("existErr", "Error: user does not exist!");
