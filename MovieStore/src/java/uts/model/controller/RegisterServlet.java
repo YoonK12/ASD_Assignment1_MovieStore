5af83bc8ea9f3f6c1ad73debb53942725d7ccd68
Heeyoon Koo <Heeyoon.koo-1@student.uts.edu.au>
2020-09-15 15:07:21 +0900

    Update RegisterServlet.java

--- MovieStore/src/java/uts/model/controller/RegisterServlet.java
+++ MovieStore/src/java/uts/model/controller/RegisterServlet.java
@@ -34,28 +34,34 @@ public class RegisterServlet extends HttpServlet {
         String password = request.getParameter("password");
         String name = request.getParameter("name");
         String DOB = request.getParameter("DOB");
-        int gender = request.getInt(gender);
-        boolean active = request.getBoolean(active);
+        String gender = request.getParameter("gender");
         
+        int registered_gen;
+        
+        if("male".equals(gender)){
+            registered_gen = 0;
+        } else {
+            registered_gen = 1;
+        }
+   
         
         CusManager cusManager = (CusManager) session.getAttribute("manager");
         Validator validator = new Validator();
         
         // validate email
-        if (!validator.validateEmail(email)) {           
+        if (!validator.validateEmail(cEmail)) {           
 
             // set incorrect email error to the session   
-            session.setAttribute("errMsg", "Error: email format incorrect");
+            session.setAttribute("emailErr", "Error: Email format incorrect");
 
             // redirect user back to the login.jsp
             request.getRequestDispatcher("register.jsp").include(request, response);
             
         // validate password
-        } else if (password instanceof String == false) {                  
-            
-            // set incorrect password error to the session           
-            session.setAttribute("errMsg", "Error: password format incorrect");
-            
+        } else if (!validator.validatePassword(password)) {
+            // set incorrect password error to the session 
+            session.setAttribute("passErr", "Requires at least 6 characters including upper or lower alpha and digit");
+
             // redirect user back to the login.jsp          
             request.getRequestDispatcher("register.jsp").include(request, response);
         
@@ -71,9 +77,10 @@ public class RegisterServlet extends HttpServlet {
         } else {
         
             try {
+                //Customer(String cEmail, String password, String cusName,String DOB, int gender, boolean active)
                 if (cusManager.findCustomer(cEmail) == null) {
-                    cusManager.addCustomer(cEmail, password, name, DOB, gender, active);
-                    Customer customer = new Customer(cEmail, name, password, gender, active);
+                    cusManager.addCustomer(cEmail, password, name, DOB, registered_gen, true);
+                    Customer customer = new Customer(cEmail, password, name, DOB, registered_gen, true);
                     session.setAttribute("customer", customer);
 
                     // redirect to the next page
