f36771cefb27cc7f2bbb139b8563057fb8431ae4
nneeseong <54238922+neeseong@users.noreply.github.com>
2020-09-27 02:18:55 +1000

    Update ViewMainServlet.java
    - The second item puts in the cart, removeAttribute of session "addedMovie"
    - and then, setAttribute("cart", cart)
    - and then Go to cart.jsp page

--- MovieStore/src/java/uts/model/controller/ViewMainServlet.java
+++ MovieStore/src/java/uts/model/controller/ViewMainServlet.java
@@ -7,18 +7,13 @@
 package uts.model.controller;
 
 import java.io.IOException;
-import java.io.PrintWriter;
-import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import uts.asd.dao.DBMovie;
-import uts.asd.model.*;
+import uts.asd.model.Cart;
 
 /**
  *
@@ -29,29 +24,17 @@ public class ViewMainServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        int movieID = 0;
-        try{
-            movieID = Integer.parseInt(request.getParameter("movieID"));
-        }catch (NumberFormatException e) {
-            
-        }
         
         HttpSession session = request.getSession();
-        Validator validator = new Validator();
-        String title = request.getParameter("title");
-        String director = request.getParameter("director");
-        String description = request.getParameter("description"); 
-        String price = request.getParameter("price");
-        String released_date = request.getParameter("released_date");
-        String category = request.getParameter("category"); 
-        DBMovie movieManager = (DBMovie) session.getAttribute("movieManager");
         
-        try {
-            ArrayList<Movie> display = movieManager.fetchMovie();
-            
-        } catch (SQLException ex) {
-            Logger.getLogger(ViewMainServlet.class.getName()).log(Level.SEVERE, null, ex);
-        }
+        Cart cart = (Cart)session.getAttribute("cart");
+        DBMovie movie = (DBMovie) session.getAttribute("addedMovie");
+        cart.addToCart(movie,1);
+        session.removeAttribute("addedMovie");
+        session.setAttribute("cart", cart);
+
+        response.sendRedirect("cart.jsp"); // Cart page
+
     }
 
 }
