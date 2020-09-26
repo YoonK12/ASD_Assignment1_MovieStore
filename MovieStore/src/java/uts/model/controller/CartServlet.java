67854da629aea950d1e12ee7b80dcc41f052b7ba
nneeseong <54238922+neeseong@users.noreply.github.com>
2020-09-27 02:19:20 +1000

    Create CartServlet

--- MovieStore/src/java/uts/model/controller/CartServlet.java
+++ MovieStore/src/java/uts/model/controller/CartServlet.java
@@ -4,23 +4,13 @@
  * and open the template in the editor.
  */
 package uts.model.controller;
-import uts.asd.model.User;
 
 import java.io.IOException;
-import java.io.PrintWriter;
-import java.sql.SQLException;
-import java.util.Date;
-import java.util.List;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
-import uts.asd.dao.DBConnector;
-import uts.asd.dao.DBOrder;
-import uts.asd.model.Order;
-
 /**
  *
  * @author heeseong
