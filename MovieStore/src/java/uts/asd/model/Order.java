9e2e8103d3e7b7764018269aec34c3dd36b51ba1
nneeseong <54238922+neeseong@users.noreply.github.com>
2020-09-28 03:54:49 +1000

    create DAO and java bean for Order

--- MovieStore/src/java/uts/asd/model/Order.java
+++ MovieStore/src/java/uts/asd/model/Order.java
@@ -5,10 +5,96 @@
  */
 package uts.asd.model;
 
+import java.util.Date;
+
 /**
  *
  * @author heeseong
  */
 public class Order {
+    private Integer orderID; //PK, U, NN,
+    private Date order_date;
+    private String shipping_address;
+    private String recipient_name;
+    private String recipient_phone;
+    private String payment_method;
+    private Float total;
+    private String status;
+    
+    public Order() {
+        super();
+    }
+
+    public Order(Integer orderID, Date order_date, Float total, String status) {
+        this.orderID = orderID;
+        this.order_date = order_date;
+        this.total = total;
+        this.status = status;
+    }
+
+    public Integer getOrderID() {
+        return orderID;
+    }
+
+    public void setOrderID(Integer orderID) {
+        this.orderID = orderID;
+    }
+
+    public Date getOrder_date() {
+        return order_date;
+    }
+
+    public void setOrder_date(Date order_date) {
+        this.order_date = order_date;
+    }
+
+    public String getShipping_address() {
+        return shipping_address;
+    }
+
+    public void setShipping_address(String shipping_address) {
+        this.shipping_address = shipping_address;
+    }
+
+    public String getRecipient_name() {
+        return recipient_name;
+    }
+
+    public void setRecipient_name(String recipient_name) {
+        this.recipient_name = recipient_name;
+    }
+
+    public String getRecipient_phone() {
+        return recipient_phone;
+    }
+
+    public void setRecipient_phone(String recipient_phone) {
+        this.recipient_phone = recipient_phone;
+    }
+
+    public String getPayment_method() {
+        return payment_method;
+    }
+
+    public void setPayment_method(String payment_method) {
+        this.payment_method = payment_method;
+    }
+
+    public Float getTotal() {
+        return total;
+    }
+
+    public void setTotal(Float total) {
+        this.total = total;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+    
     
 }
